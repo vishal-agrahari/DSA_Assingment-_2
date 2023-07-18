0947eb117fdd407056032a3f2155b5f664ccee18
Vishal Agrahari <128042149+vishal-agrahari@users.noreply.github.com>
2023-07-18 10:45:17 -0700

    Update answer1.js

--- answer1.js
+++ answer1.js
@@ -1,13 +1,18 @@
-def array_pair_sum(nums):
-    nums.sort()
-    max_sum = 0
+function arrayPairSum(nums) {
+  // Sort the array in ascending order
+  nums.sort((a, b) => a - b);
 
-    for i in range(0, len(nums), 2):
-        max_sum += nums[i]
+  let maxSum = 0;
 
-    return max_sum
+  for (let i = 0; i < nums.length; i += 2) {
+    // Pair adjacent elements and sum the minimum of each pair
+    maxSum += Math.min(nums[i], nums[i + 1]);
+  }
 
-# Test Example 1
-nums = [1, 4, 3, 2]
-result = array_pair_sum(nums)
-print(result)  # Output: 4
+  return maxSum;
+}
+
+// Test Example 1
+const nums = [1, 4, 3, 2];
+const result = arrayPairSum(nums);
+console.log(result); // Output: 4
